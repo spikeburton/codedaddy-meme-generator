bfeeb35c8db487490a7a4bdc21116b12d8d9ac15
spikeburton <ralphburton3@gmail.com>
2019-03-10 15:12:32 -0400

    add generate function

--- js/index.js
+++ js/index.js
@@ -1,3 +1,18 @@
+const sayings = [
+  {
+    top: "that face when",
+    bottom: "you're a boss"
+  },
+  {
+    top: "what you know",
+    bottom: "bout bein' codedaddy"
+  }
+]
+
 function generate() {
-  window.alert("button clicked!");
+  let topText = document.getElementById("top-text");
+  let bottomText = document.getElementById("bottom-text");
+  let saying = sayings[Math.floor(Math.random() * sayings.length)];
+
+  // window.alert(`${saying.top} ${saying.bottom}`);
 }
