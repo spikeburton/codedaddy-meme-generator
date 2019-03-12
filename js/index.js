0f46276186ac34ff9b86490fc1bfc19ac4869566
Dana Simmons <dana@simmons-audio.com>
2019-03-12 11:14:07 -0400

    got the canvas and text to render

--- js/index.js
+++ js/index.js
@@ -6,3 +6,32 @@ function generate() {
   topText.innerText = saying.top;
   bottomText.innerText = saying.bottom;
 }
+
+const image = document.querySelector("img")
+const generator = document.getElementById("generator")
+
+const ctx = generator.getContext('2d')
+
+ctx.font = "36pt Verdana"
+ctx.drawImage(image,0,0)
+ctx.fillText("Test 123", 40, 80)
+
+
+// var canvas = document.getElementById('canvas'),
+//         ctx = canvas.getContext('2d');
+// canvas.width = $('img').width();
+// canvas.crossOrigin = "Anonymous";
+// canvas.height = $('img').height();
+// ctx.drawImage($('img').get(0), 0, 0);
+// ctx.font = "36pt Verdana";
+// $(document).on('input','#inp',function(){
+//     //redraw image
+//     ctx.clearRect(0,0,canvas.width,canvas.height);
+//     ctx.drawImage($('img').get(0), 0, 0);
+//     //refill text
+//     ctx.fillStyle = "red";
+//     ctx.fillText($(this).val(),40,80);
+// });
+// $('button').click(function(){
+//     console.log(ctx.getImageData(50, 50, 100, 100));
+// });
